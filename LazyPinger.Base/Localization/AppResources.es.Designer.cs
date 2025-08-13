e9e4d1f41a46799c517e1a792c81dfd3c14750c0
DZimo <53861899+DZimo@users.noreply.github.com>
2025-08-13 22:34:34 +0200

    New strings to appresources

--- LazyPinger.Base/Localization/AppResources.es.Designer.cs
+++ LazyPinger.Base/Localization/AppResources.es.Designer.cs
@@ -22,14 +22,14 @@ namespace LazyPinger.Base.Localization {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class AppResources {
+    public class AppResources___Copy {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal AppResources() {
+        internal AppResources___Copy() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace LazyPinger.Base.Localization {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LazyPinger.Base.Localization.AppResources", typeof(AppResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LazyPinger.Base.Localization.AppResources - Copy", typeof(AppResources___Copy).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -61,7 +61,7 @@ namespace LazyPinger.Base.Localization {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Hola senior.
+        ///   Looks up a localized string similar to LazyPinger, as lazy as possible.
         /// </summary>
         public static string LazySlogan {
             get {
