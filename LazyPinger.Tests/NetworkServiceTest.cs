436b30d407003ed9832c73ca3c2a437aa7508d1b
DZimo <53861899+DZimo@users.noreply.github.com>
2025-08-13 22:34:08 +0200

    Add of tests project with network example

---
+++ LazyPinger.Tests/NetworkServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using LazyPinger.Core.Services;
+
+namespace LazyPinger.Tests
+{
+    [TestClass]
+    public sealed class NetworkServiceTest
+    {
+        [TestMethod]
+        public void InitNetworkSettingsTest()
+        {
+            var networkService = new NetworkService();
+            var res = networkService.InitNetworkSettings();
+            Assert.IsNotNull(res);
+        }
+    }
+}
