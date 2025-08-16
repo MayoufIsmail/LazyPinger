268ee4067dd10ef5581e2e18690ae0f0a4cc7a47
DZimo <53861899+DZimo@users.noreply.github.com>
2025-08-16 16:56:12 +0200

    New migrations for tables update

---
+++ LazyPinger.Base/Migrations/20250816135449_FixedUserSelections.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LazyPinger.Base.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedUserSelections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DevicesGroups",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Type = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevicesGroups", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSelections",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AutoRun = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FastPing = table.Column<bool>(type: "INTEGER", nullable: false),
+                    FastnessLevel = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSelections", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DevicePings",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DevicesGroupID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    IP = table.Column<string>(type: "TEXT", nullable: false),
+                    Port = table.Column<string>(type: "TEXT", nullable: false),
+                    MacAddress = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<string>(type: "TEXT", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: false),
+                    AnswerTime = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DevicePings", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_DevicePings_DevicesGroups_DevicesGroupID",
+                        column: x => x.DevicesGroupID,
+                        principalTable: "DevicesGroups",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DevicePings_DevicesGroupID",
+                table: "DevicePings",
+                column: "DevicesGroupID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DevicePings");
+
+            migrationBuilder.DropTable(
+                name: "UserSelections");
+
+            migrationBuilder.DropTable(
+                name: "DevicesGroups");
+        }
+    }
+}
