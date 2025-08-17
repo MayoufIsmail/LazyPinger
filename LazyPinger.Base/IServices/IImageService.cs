4fe346ebd9b17711e7c73377ddb806b7a53b75ca
DZimo <53861899+DZimo@users.noreply.github.com>
2025-08-17 20:42:17 +0200

    ImageService implementation, for devices pictures management

---
+++ LazyPinger.Base/IServices/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace LazyPinger.Base.IServices
+{
+    public interface IImageService<T>
+    {
+        public Task<byte[]> ImageToBytes(T image);
+
+        public T BytesToImage(byte[] data);
+
+        public Task ResizeImage(int width, int height);
+    }
+}
---
+++ LazyPinger.MAUI/Services/ImageService.cs
@@ -0,0 +1,26 @@
+﻿using LazyPinger.Base.IServices;
+
+namespace LazyPinger.Core.Services
+{
+    public class ImageService : IImageService<ImageSource>
+    {
+        public ImageSource BytesToImage(byte[] data)
+        {
+            return ImageSource.FromStream(() => new MemoryStream(data));
+        }
+
+        public async Task<byte[]> ImageToBytes(ImageSource image)
+        {
+            using var ms = new MemoryStream();
+
+            await ms.CopyToAsync(ms);
+            var imageBytes = ms.ToArray();
+            return imageBytes;
+        }
+
+        public Task ResizeImage(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
