ec3d4d20046c435ad6dfd2fa11941a18bd81bfcd
DZimo <53861899+DZimo@users.noreply.github.com>
2025-08-19 22:42:00 +0200

    Updated listen VM to observables

--- LazyPinger.Core/ViewModels/ListenVm.cs
+++ LazyPinger.Core/ViewModels/ListenVm.cs
@@ -1,11 +1,11 @@
-﻿using LazyPinger.Base.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LazyPinger.Base.Models;
 using LazyPinger.Base.Models.Devices;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.ObjectModel;
 
 namespace LazyPinger.Core.ViewModels
 {
-    public class ListenVm
+    public class ListenVm : ObservableObject
     {
         private static ListenVm? instance;
         public static ListenVm Instance => ListenVm.instance ??= new ListenVm();
@@ -14,91 +14,148 @@ namespace LazyPinger.Core.ViewModels
 
         private ListenVm() { }
 
+        public IEnumerable<DevicesGroup> devicesGroup;
         public IEnumerable<DevicesGroup> DevicesGroup
         {
             get
             {
-                return GetDevicesGroup();
+                return devicesGroup;
+            }
+            set
+            {
+                devicesGroup = value;
+                OnPropertyChanged(nameof(DevicesGroup));
+                OnPropertyChanged(nameof(DevicesPing));
             }
         }
 
+        private IEnumerable<DevicePing> devicesPing;
         public IEnumerable<DevicePing> DevicesPing
         {
             get
             {
-                return GetDevicePing();
+                return devicesPing;
+            }
+            set
+            {
+                devicesPing = value;
+                OnPropertyChanged(nameof(DevicesPing));
+                OnPropertyChanged(nameof(DevicesGroup));
             }
         }
 
+        private IEnumerable<VmDevicesGroup> devicesGroupVm;
         public IEnumerable<VmDevicesGroup> DevicesGroupVm
         {
             get
             {
-                return GetDevicesGroupVm();
+                return devicesGroupVm;
+            }
+            set
+            {
+                devicesGroupVm = value;
+                OnPropertyChanged(nameof(DevicesPing));
+                OnPropertyChanged(nameof(DevicesGroup));
+                OnPropertyChanged(nameof(DevicesGroupVm));
             }
         }
 
+        private VmUserSelection? userSelectionsVm { get; set; }
         public VmUserSelection? UserSelectionsVm
         {
-            get
+            get 
             {
-                return GetUserSelectionVm();
+                return userSelectionsVm;
+            }
+            set
+            {
+                userSelectionsVm = value;
+                OnPropertyChanged(nameof(UserSelectionsVm));
+                OnPropertyChanged(nameof(DevicesPing));
             }
         }
 
-
-        public List<DevicesGroup> GetDevicesGroup()
+        public Action GetDevicesGroup()
         {
-            try
+            return () =>
             {
-                return dbContext.DevicesGroups.Include(o => o.DevicePings).ToList();
-            }
-            catch (Exception ex)
-            {
-                return new();
-            }
-        }
-
-        public List<VmDevicesGroup> GetDevicesGroupVm()
-        {
-            try
-            {
-                return dbContext.DevicesGroups.Include(o => o.DevicePings).ToList().Select((o) => new VmDevicesGroup(o)
+                try
                 {
-                    Color = o.Color,
-                    Type = o.Type
-                }).ToList();
-            }
-            catch (Exception ex)
-            {
-                return new();
-            }
+                    var res = dbContext.DevicesGroups.Include(o => o.DevicePings).ToList();
+                    DevicesGroup = res;
+                    Instance.DevicesGroupVm = res.Select(o => new VmDevicesGroup(o));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load DevicesGroup from Database: {ex.Message}");
+                }
+            };
         }
 
-        public List<DevicePing> GetDevicePing()
+        public Action GetDevicesGroupVm()
         {
-            try
+            return () =>
             {
-                return dbContext.DevicePings.Include(o => o.DevicesGroup).ToList() ?? new List<DevicePing>();
-            }
-            catch (Exception ex)
-            {
-                return new();
-            }
+                try
+                {
+                    var res = dbContext.DevicesGroups.Include(o => o.DevicePings).ToList().Select((o) => new VmDevicesGroup(o)
+                    {
+                        Color = o.Color,
+                        Type = o.Type
+                    }).ToList();
+
+                    DevicesGroupVm = res;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load DevicesGroup from Database: {ex.Message}");
+                }
+            };
         }
 
-        public VmUserSelection? GetUserSelectionVm()
+        public Action GetDevicePing()
         {
-            try
+            return () =>
             {
-                var res = dbContext.UserSelections.FirstOrDefault();
-                if (res == null) return null;
-                return new VmUserSelection(res);
-            }
-            catch (Exception ex)
+                try
+                {
+                    var res = dbContext.DevicePings.Include(o => o.DevicesGroup).ToList();
+                    DevicesPing = res;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load DevicePing from Database: {ex.Message}");
+                }
+            };
+        }
+
+        public Action GetUserSelectionVm()
+        {
+            return async ()  =>
             {
-                return null;
-            }
+                try
+                {
+                    var res = await dbContext.UserSelections.FirstAsync();
+                    UserSelectionsVm = new VmUserSelection(res);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to load UserSelections from Database: {ex.Message}");
+                }
+            };
+        }
+
+        public static void ReloadFromDatabase(object type)
+        {
+            var action = type switch
+            {
+                VmUserSelection vmUserSelection => Instance.GetUserSelectionVm(),
+                DevicesGroup devicesGroup => Instance.GetDevicesGroup(),
+                VmDevicesGroup devicePing => Instance.GetDevicesGroupVm(),
+                DevicePing devicePing => Instance.GetDevicePing(),
+            };
+
+            action.Invoke();
         }
     }
 }
